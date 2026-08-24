e57ac2ff4648c4c26456aa8efe79b520dd220b07
sakilkhan649 <mdsakilkhan649@gmail.com>
2026-08-24 08:15:53 +0600

    feat: add ProfileAvatarLeading widget for profile navigation

--- lib/core/widgets/profile_avatar_leading.dart
+++ lib/core/widgets/profile_avatar_leading.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 import '../../features/auth/controllers/auth_controller.dart';
 import '../../features/profile/views/profile_screen.dart';
@@ -25,14 +26,39 @@ class ProfileAvatarLeading extends StatelessWidget {
         },
         child: Padding(
           padding: EdgeInsets.only(left: 16.w),
-          child: CircleAvatar(
-            backgroundColor: Colors.white.withValues(alpha: 0.2),
-            backgroundImage: user.photoUrl != null && user.photoUrl!.isNotEmpty
-                ? NetworkImage(user.photoUrl!)
-                : null,
-            child: user.photoUrl == null || user.photoUrl!.isEmpty
-                ? Icon(Icons.person, color: Colors.white, size: 20.r)
-                : null,
+          child: Container(
+            width: 40.r,
+            height: 40.r,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white.withValues(alpha: 0.2),
+            ),
+            clipBehavior: Clip.antiAlias,
+            child: user.photoUrl != null && user.photoUrl!.isNotEmpty
+                ? CachedNetworkImage(
+                    imageUrl: user.photoUrl!,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) => Center(
+                      child: SizedBox(
+                        width: 20.r,
+                        height: 20.r,
+                        child: const CircularProgressIndicator(
+                          color: Colors.white,
+                          strokeWidth: 2,
+                        ),
+                      ),
+                    ),
+                    errorWidget: (context, url, error) => Icon(
+                      Icons.person,
+                      color: Colors.white,
+                      size: 20.r,
+                    ),
+                  )
+                : Icon(
+                    Icons.person,
+                    color: Colors.white,
+                    size: 20.r,
+                  ),
           ),
         ),
       );
